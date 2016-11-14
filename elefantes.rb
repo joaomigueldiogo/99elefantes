7e1db4c453631fb14bddb89a5f297ff98a8c1f3c
codeamac06 <codeamac06@gmail.com>
2016-11-14 10:40:23 -0600

    song loop

---
+++ elefantes.rb
@@ -0,0 +1,17 @@
+# Tienes que escribir un programa que modele la canción de los elefantes del 1 al 99. 
+
+# Utiliza rangos y loops. Haz que tu programa pueda recibir el número de elefantes en el que se rome la telaraña. 
+
+# Juega con la diferencia de utilizar puts y print. 
+
+# En USA cantan una canción similar que se llama “99 bottles of beer”, la diferencia es que el conteo va en reversa. 
+
+# Busca una manera de hacer un loop fácilmente y para ello existe un método.
+
+  contador_song = 0
+
+  
+while contador_song < 100 do 
+  puts "#{contador_song} elefante se columpiaba sobre la tela de una araña, como veía que resistía fueron a llamar a otro elefante."
+  contador_song += 1
+end
